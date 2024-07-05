dbf0784a68bb72740de2f33ec33ab8f2ae808dd7
jean-philippe.breysse <jeanphi@jeanphiippesair.home>
2024-07-05 17:57:52 +0200

    first commit

---
+++ createtable.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100)
+);
+
+SELECT * FROM users;
+
---
+++ docker.sh
@@ -0,0 +1,4 @@
+docker run --name my-postgres-container -e POSTGRES_USER=myuser -e POSTGRES_PASSWORD=mypassword -e POSTGRES_DB=mydatabase -p 5432:5432 -d postgres
+
+
+docker run --name my-postgres-container -e POSTGRES_USER=myuser -e POSTGRES_PASSWORD=mypassword -e POSTGRES_DB=mydatabase -p 5432:5432 -d postgres
